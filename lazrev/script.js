5aba8b564e45c81c2c0ba2e5dd18052404fdca86
dipeshjoshi <dipeshj2310@gmail.com>
2024-05-14 15:47:38 +0530

    add

--- lazrev/script.js
+++ lazrev/script.js
@@ -1,42 +1,46 @@
-var nav = document.querySelector("nav");
 
-nav.addEventListener("mouseenter", function () {
-    
-    var tl = gsap.timeline()
+function navAnimation() {
+    var nav = document.querySelector("nav");
 
-    tl.to("#nav-bottom", {
-        height: "21vh",
-        duration: 0.7
-    })
-    tl.to(".nav-part2 h5", {
-        display: "block",
-        duration:0.1
-    })
-    tl.to(".nav-part2 h5 span", {
-        y: 0,
-        // duration: 0.2,
-        stagger: {
-            amount: 0.5
-        }
-    })
-})
+    nav.addEventListener("mouseenter", function () {
 
-nav.addEventListener("mouseleave", function () {
+        let tl = gsap.timeline()
 
-    let tl = gsap.timeline()
+        tl.to("#nav-bottom", {
+            height: "21vh",
+            duration: 0.7
+        })
+        tl.to(".nav-part2 h5", {
+            display: "block",
+            duration: 0.1
+        })
+        tl.to(".nav-part2 h5 span", {
+            y: 0,
+            stagger: {
+                amount: 0.5
+            }
+        })
+    })
 
-    tl.to(".nav-part2 h5 span", {
-        y: 25,
-        stagger: {
-            amount: 0.2
-        }
+    nav.addEventListener("mouseleave", function () {
+
+        let tl = gsap.timeline()
+
+        tl.to(".nav-part2 h5 span", {
+            y: 25,
+            stagger: {
+                amount: 0.2
+            }
+        })
+        tl.to(".nav-part2 h5", {
+            display: "none",
+            duration: 0.1
+        })
+        tl.to("#nav-bottom", {
+            height: 0,
+            duration: 0.2
+        })
     })
-    tl.to(".nav-part2 h5", {
-        display: "none",
-        duration: 0.1
-    })
-    tl.to("#nav-bottom", {
-        height: 0,
-        duration: 0.2
-    })
-})
+}
+
+navAnimation();
